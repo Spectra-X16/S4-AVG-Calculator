be0359c4088f9dfab4da89a79908c81e20a7c778
Mohammed Ilyas <ilyesnakh8@gmail.com>
2026-05-13 18:34:42 +0100

    Update app S4.py

--- S4 CALC/app S4.py
+++ S4 CALC/app S4.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="S4 Average Calculator", page_icon="🎓")
 
-st.title("🎓 Fourth Semester Average Calculator — Automation Field")
+st.title("🎓 S4 Average Calculator — Automation Field")
 
 # 1. Get the User's Name
 name = st.text_input("Please Write Your Name:", placeholder="e.g. Mohammed")
